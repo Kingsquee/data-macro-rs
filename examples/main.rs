5c022b25bc605e774a00f2b708c34da98890e47c
Kingsley Tolmie <kingsley.tolmie@gmail.com>
2014-12-29 22:16:49 -0800

    Initial commit.

---
+++ examples/main.rs
@@ -0,0 +1,21 @@
+#![feature(phase)]
+#[phase(plugin)]
+extern crate data_macro;
+
+data!(
+    SomeData {
+        a: uint = 1             // fields can be set as normal
+        b: uint = a + 2         // fields can reference other fields
+        c: uint = mul_by_four(b)// fields can be set via functions
+    }                           // commas and semicolons are unnecessary
+);
+
+fn mul_by_four(num: uint) -> uint {
+    println!("Multiplying {} by 4", num);
+    num * 4
+}
+
+fn main() {
+    let data: SomeData = SomeData::new();
+    println!("data.c is {}", data.c);
+}
---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+#![feature(macro_rules)]
+#![macro_escape]
+
+#[macro_export]
+macro_rules! data {
+    ($structtype:ident {$($var:ident: $vartype:ty = $val:expr)+}) => {
+        #[deriving(Copy)]
+        pub struct $structtype {
+            $(
+                pub $var: $vartype,
+            )+
+        }
+
+        impl $structtype {
+            pub fn new() -> $structtype {
+                $(
+                    let $var = $val;
+                )+
+                $structtype {
+                    $(
+                        $var: $var,
+                    )+
+                }
+            }
+        }
+    }
+}
