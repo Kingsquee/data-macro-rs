e91f9d45aed8ad645569fe66c41cf9ea50d27ccb
Kingsley Tolmie <kingsley.tolmie@gmail.com>
2015-01-05 10:27:22 -0800

    Removed #[deriving(Copy)]

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@
 #[macro_export]
 macro_rules! data {
     ($structtype:ident {$($var:ident: $vartype:ty = $val:expr)+}) => {
-        #[deriving(Copy)]
         pub struct $structtype {
             $(
                 pub $var: $vartype,
